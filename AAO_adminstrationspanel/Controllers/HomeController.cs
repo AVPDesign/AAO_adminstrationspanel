83e1012b228ba331c96119c9573e92b74ecae532
AVPDesign <andreasvp@hotmail.dk>
2021-11-04 10:50:41 +0100

    Add project files.

---
+++ AAO_adminstrationspanel/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using AAO_adminstrationspanel.Models;
+
+namespace AAO_adminstrationspanel.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult OpretTur()
+        {
+            return View();
+        }
+
+        public IActionResult AlleTure()
+        {
+            return View();
+        }
+
+        public IActionResult TildelteTure()
+        {
+            return View();
+        }
+
+        public IActionResult AfloserOversigt()
+        {
+            return View();
+        }
+    }
+}
---
+++ AAO_adminstrationspanel/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace AAO_adminstrationspanel.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
