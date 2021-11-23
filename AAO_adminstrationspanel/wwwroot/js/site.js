38b19d68e280251ccc41b1ca20df8c1e5e9637fe
Amalie Lauritsen <42379939+AmalieLauritsen@users.noreply.github.com>
2021-11-23 12:35:50 +0100

    edit

--- AAO_adminstrationspanel/wwwroot/js/site.js
+++ AAO_adminstrationspanel/wwwroot/js/site.js
@@ -3,13 +3,31 @@ var expandArrow = document.querySelector(".expandArrow")
 var expandMore = document.querySelector(".expandMore")
 var expandLess = document.querySelector(".expandLess")
 
-function tableOpenClose() {
-    tableToOpen.style.display = "revert";
-    expandArrow.classList.remove("expandMore");
-    expandArrow.classList.add("expandLess");
+//function tableOpenClose() {
+//    tableToOpen.style.display = "revert";
+//    expandArrow.classList.remove("expandMore");
+//    expandArrow.classList.add("expandLess");
 //    if (tableToOpen.style.display = "revert") {
 //        tableToOpen.style.display = "none";
 //        expandArrow.classList.remove("expandLess");
 //        expandArrow.classList.add("expandMore");
 //    }
-}
+//}
+
+
+
+
+
+const expandRows = document.querySelectorAll('.expandRow');
+
+expandRows.forEach(expandRow => {
+    expandRow.addEventListener('click', function() {
+        if (!expandRow.classList.contains('expandLess')) {
+            expandRow.classList.add('expandLess');
+            expandRow.nextElementSibling.classList.add('tableToOpenExpanded');
+        } else {
+            expandRow.classList.remove('expandLess');
+            expandRow.nextElementSibling.classList.remove('tableToOpenExpanded');
+        }
+    });
+});
