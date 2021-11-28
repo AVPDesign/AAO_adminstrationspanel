f223014e0840b35fc5b6548af8af2e11594b4ccc
Amalie Lauritsen <42379939+AmalieLauritsen@users.noreply.github.com>
2021-11-28 12:29:05 +0100

    Selected table

--- AAO_adminstrationspanel/wwwroot/js/site.js
+++ AAO_adminstrationspanel/wwwroot/js/site.js
@@ -1,5 +1,17 @@
-﻿document.querySelectorAll('.expandRow').forEach(expandRow => {
-    expandRow.addEventListener('click', function() {
+﻿//document.querySelectorAll('.expandRow').forEach(expandRow => {
+//    expandRow.addEventListener('click', function() {
+//        if (!expandRow.classList.contains('expandLess')) {
+//            expandRow.classList.add('expandLess');
+//            expandRow.nextElementSibling.classList.add('tableToOpenExpanded');
+//        } else {
+//            expandRow.classList.remove('expandLess');
+//            expandRow.nextElementSibling.classList.remove('tableToOpenExpanded');
+//        }
+//    });
+//});
+
+document.querySelectorAll('.expandRow').forEach(expandRow => {
+    expandRow.addEventListener('click', function () {
         if (!expandRow.classList.contains('expandLess')) {
             expandRow.classList.add('expandLess');
             expandRow.nextElementSibling.classList.add('tableToOpenExpanded');
