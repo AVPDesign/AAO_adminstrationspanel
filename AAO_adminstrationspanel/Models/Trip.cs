21f063f138488939b2117e1c4cce25ddafc03363
AVPDesign <60422075+AVPDesign@users.noreply.github.com>
2021-12-10 11:56:18 +0100

    how det her er ikke min branch...

--- AAO_adminstrationspanel/Models/Trip.cs
+++ AAO_adminstrationspanel/Models/Trip.cs
@@ -14,16 +14,40 @@ namespace AAO_adminstrationspanel.Models
         }
 
         public int Id { get; set; }
+
         [Required]
         [Display(Name = "Startdato")]
         public DateTime? StartDate { get; set; }
+
+        [Required]
+        [Display(Name = "Slutdato")]
         public DateTime? EndDate { get; set; }
+
+        [Display(Name = "Hastetur")]
         public bool? Priority { get; set; }
+
+        [Required]
+        [Display(Name = "Varighed")]
         public int? TravelTime { get; set; }
+
+        [Required]
+        [Display(Name = "Beskrivelse")]
         public string Description { get; set; }
+
+        [Required]
+        [Display(Name = "Kontakt")]
         public int? ContactId { get; set; }
+
+        [Required]
+        [Display(Name = "Afdeling")]
         public int? DepartmentId { get; set; }
+
+        [Required]
+        [Display(Name = "Trafik fra")]
         public int? StartCountryId { get; set; }
+
+        [Required]
+        [Display(Name = "Trafik til")]
         public int? EndCountryId { get; set; }
 
         public virtual User Contact { get; set; }
